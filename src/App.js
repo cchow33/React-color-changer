641f022a21f1495b02bf0b38c1604de6fbeed19d
Carmen Chow <carmendevelops@gmail.com>
2022-12-22 13:04:32 -0500

    add styling

--- src/App.js
+++ src/App.js
@@ -3,21 +3,42 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App"
+        style={{
+          'display': 'flex',
+          'alignItems': 'center',
+          'justifyContent': 'center',
+        }}>
+      
+      <div className="color-container"
+        style={{
+          'position': 'relative',
+          'marginTop': '10%'
+        }}>
+
+        <div className='color-square'
+          style={{ 
+            'width': '200px',
+            'height': '200px',
+            'borderRadius': '10px',
+            'border': '1px solid black'
+          }}>
+          <p className="label">Empty Value</p>
+        </div>
+
+        <input 
+          type="text"
+          placeholder='Add color name'
+          style={{
+            'width': '195px',
+            'height': '40px',
+            'borderRadius': '10px',
+            'marginTop': '10px'
+          }}  
         >
-          Learn React
-        </a>
-      </header>
+        </input>
+
+        </div>
     </div>
   );
 }
